543d4de3182518ff2452617a13ac3f76d8699299
Youkin06 <151609740+Youkin06@users.noreply.github.com>
2025-06-11 14:51:47 +0900

    ステージ4初期設定

--- Assets/Scripts/StageFourController.cs
+++ Assets/Scripts/StageFourController.cs
@@ -1,15 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class StageFourController : MonoBehaviour
 {
+    [Header("stage1管理")]
+    public int stage4Num_i = 0;
+
+    [Header("UI関連")]
+    public GameObject Date_obj;
+    public TextMeshProUGUI Date_txt;
+    public string[] Date_string;
+    public GameObject NikkiContent_obj;
+    public TextMeshProUGUI NikkiContent_txt;
+    public string[] NikkiContent_string;
+
+    [Header("サウンド")]
+    public AudioSource noise_sound;
+    public AudioSource writing_sound;
+    [Header("ステージ4オブジェクト")]
+    public GameObject room_stage4_obj;
+    public GameObject roomShadow_obj;
+    [Header("プレイヤー情報")]
+    public GameObject Player_obj;
+    public GameObject PlayerShadow_obj;
+    public PlayerController PC;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player_obj = GameObject.FindWithTag("Player");
+        PC = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+        noise_sound.Play();
+        StartCoroutine(StartScene());
     }
 
+    IEnumerator StartScene()
+    {
+
+    }
     // Update is called once per frame
     void Update()
     {
